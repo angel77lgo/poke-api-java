333c5a4ac89a01b7bd78638a5422ea3da4fa3cd6
angel77lgo <angel.de.oz.97@gmail.com>
2024-11-14 19:06:26 -0600

    init proyect

---
+++ src/main/java/luis/sanchez/poke_api_java/PokeApiJavaApplication.java
@@ -0,0 +1,13 @@
+package luis.sanchez.poke_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luis/sanchez/poke_api_java/PokeApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package luis.sanchez.poke_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
